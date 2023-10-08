c87d605f472c93ad1f9795858d0190f044089760
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-10-08 17:10:03 +0530

    clerk setup

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div className="h-full flex justify-center items-center">
       <Button>click me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
