85df36e2551675ba76a1264b3c00ca5034a591a7
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-10-24 12:19:44 +0530

    dynamic layout

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnalyticsPage = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      Analytics page
+    </div>
+  );
+};
+
+export default AnalyticsPage;
---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,7 @@
+const CoursesPage = () => {
+  return (
+    <div className="h-full flex justify-center items-center">Courses page</div>
+  );
+};
+
+export default CoursesPage;
